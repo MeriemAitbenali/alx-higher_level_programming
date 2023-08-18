4af68ce0b113355e78300d5c0adbb1bfb4a56d44
meriem.aitbenali06 <meriem.aitbenali06@gmail.com>
2023-08-18 07:14:24 -0700

    replace

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to compute the square value of all integers of a matrix
-#
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# -----------------------------------------------------------
+square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
 
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
 
-def square_matrix_simple(matrix=[]):
-    new_matrix = [[number**2 for number in row] for row in matrix]
-    return new_matrix
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
 
