5078c5b8db2cc83c9eecbd9547d4ce614c4452ed
meriem.aitbenali06 <meriem.aitbenali06@gmail.com>
2023-08-18 07:50:38 -0700

    add

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,19 @@
+
+
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to add all unique integers in a list (only once
+# for each integer)
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# -----------------------------------------------------------
+
+
+def uniq_add(my_list=[]):
+    my_set = set(my_list)
+    sum = 0
+    for number in my_set:
+        sum += number
+    return sum
+
